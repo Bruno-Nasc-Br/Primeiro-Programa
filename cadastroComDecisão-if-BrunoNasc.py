33e2b5adc18470f331f1a39025f3b4e4e1ef41cf
franklinnascimentolopesjr2 <35469187+franklinnascimentolopesjr2@users.noreply.github.com>
2018-01-16 23:18:21 -0200

    Nova estrura e min de caracteres para senha
    Usando funcoes para organizar melhor o codigo;
    Senha deve ter no minimo 8 caracteres para que o cadastro possa ser efetuado

--- cadastroComDecisão-if-BrunoNasc.py
+++ cadastroComDecisão-if-BrunoNasc.py
@@ -18,22 +18,43 @@
 #      futuras manutenção)
 
 
+# dados que serao manipulados
+# variaveis globais
+nome    = ''    # nome do cliente
+snome   = ''    # sobrenome do usuario
+login   = ''    # login para acesso
+dataNas = ''    # data de nascimento
+senha   = ''    # senha para acesso
+Csenha  = ''    # confirmacao de senha
+#tam_min_senha = 8  # tamanho minimo para senha
 
 
-########## INICIO DO PROGRAMA ##########
-print ("-------------------------------------------")
-print ("-------BEM VINDO! Tela de cadastro---------")
-print ("-------------------------------------------")
-print ()
-nome = str(input("Seu primeiro nome: "))
-snome = str(input("Seu segundo nome: "))
-login = str(input("Escolha o seu login: "))
-dataNas = input("Sua data de Nascimento. Ex: dd/mm/ano. : ")
-senha = str(input("Escolhao sua senha: "))
-Csenha = str(input("Confirme sua senha: "))
-print ()
+########## MENU PARA CAPTURAR DADOS ##########
+## parametro msg: mensagem a ser exibida
+def show_menu(msg):
+    # seta como variaveis globais
+    global nome
+    global snome
+    global login
+    global dataNas
+    global senha
+    global Csenha
 
-if (senha==Csenha):
+    print ("-------------------------------------------")
+    print (msg)
+    print ("-------------------------------------------")
+    print ()
+    nome    = str(input("Seu primeiro nome: "))
+    snome   = str(input("Seu segundo nome: "))
+    login   = str(input("Escolha o seu login: "))
+    dataNas = input("Sua data de Nascimento. Ex: dd/mm/ano. : ")
+    senha   = str(input("Escolhao sua senha: "))
+    Csenha  = str(input("Confirme sua senha: "))
+    print ()
+
+
+########## MENU PARA MOSTRAR QUE O CADASTRO FOI EFETUADO COM SUCESSO ##########
+def show_cadastro_sucesso(nome, snome, login, senha):
     print("---------------")
     print ("Senha aceita!")
     print("---------------")
@@ -42,38 +63,32 @@ if (senha==Csenha):
     print ("E sua senha é : ",senha)
     print ("ATENÇÃO: Mantenha sua senha em SEGURANÇA !!!")
 
-else:
-    print ("==================================")
-    print ("Senha não confere. Tente novamene!")
-    print("===================================")
-    print ("________Segunda tentativa_________")
-    nome = str(input("Seu primeiro nome: "))
-    snome = str(input("Seu segundo nome: "))
-    login = str(input("Escolha o seu loging: "))
-    dataNas = input("Sua data de Nascimento. Ex: dd/mm/ano. : ")
-    senha = input("Escolhao sua senha: ")
-    Csenha = input("Confirme sua senha: ")
 
-if (senha == Csenha):
-     print (" Agora seu cadastro foi concluido !")
-     print("-----------------------------------")
-     print ("Senha aceita!")
-     print ("----------------------------------")
-     print ("Bem vindo:",nome,snome)
-     print ("seu login é: ",login)
-     print ("E sua senha é: ",senha)
-     print ("ATENÇÃO: Mantenha sua senha em SEGURANÇA !!!")
+########## VERIFICA OS DADOS E 'CADASTRA' ##########
+## senha deve ter no minimo 8 caracteres
+def cadastra():
+    # seta como variaveis globais
+    global nome
+    global snome
+    global login
+    global senha
+    global Csenha
+    tam_min_senha = 8  # tamanho minimo para senha
+    tam_senha = len(senha)
 
-
-
+    if(tam_senha >= tam_min_senha):
+        if (senha==Csenha):
+            show_cadastro_sucesso(nome, snome, login, senha)
+        else:
+            show_menu("Senha não confere. Tente novamente!")
+            cadastra()
+    else:
+        show_menu("Senha deve ter no minimo 8 caracteres. Tente novamente")
+        cadastra()
 
 
-
-
-
-
-
-
-
-
-
+#############################################################
+############# INICIO DO PRORAMA #############################
+#############################################################
+show_menu("-------BEM VINDO! Tela de cadastro---------")
+cadastra()
